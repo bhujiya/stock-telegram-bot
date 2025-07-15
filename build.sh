e175a19ef408ff9935cacd50bd80154564348db6
bhujiya <yashrajjaiswal0@gmail.com>
2025-07-15 11:48:08 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-# Install system-level dependencies
-apt-get update && apt-get install -y build-essential
-
-# Pre-install setuptools so metadata errors don’t happen
-pip install setuptools==67.6.0 wheel --upgrade
-
-# Install the rest
-pip install -r requirements.txt
+pip install --upgrade pip setuptools wheel
+pip install --only-binary=:all: -r requirements.txt
