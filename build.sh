67705057702a1e04a9bd3eb955149e17b8828442
bhujiya <yashrajjaiswal0@gmail.com>
2025-07-15 12:00:48 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,9 @@ apt-get install -y build-essential python3-dev libxml2-dev libxslt-dev
 # Upgrade pip and install wheel + setuptools
 pip install --upgrade pip setuptools wheel
 
+# Install setuptools explicitly first (for pkg_resources)
+pip install setuptools>=65.0.0
+
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
